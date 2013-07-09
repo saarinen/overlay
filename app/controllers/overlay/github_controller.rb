c6b7eb0a2ba870111a0f28866fb5324256077a3b
Steve Saarinen <ssaarinen@whitepages.com>
2013-07-08 23:44:11 -0700

    Remove extra info logging

--- app/controllers/overlay/github_controller.rb
+++ app/controllers/overlay/github_controller.rb
@@ -5,14 +5,12 @@ module Overlay
     def update
       render nothing: true
 
-      logger.info "Received update post for repo: #{params[:repository]} and ref: #{params[:ref]}"
       Overlay.configuration.repositories.each do |repo_config|
-        logger.info "Processing config for repo: #{repo_config[:repo]} and branch: #{repo_config[:branch] || 'master'}"
         next unless repo_config.class == GithubRepo
         branch = repo_config[:branch] || 'master'
         if (params[:repository] && params[:ref])
           if (params[:repository][:name] == repo_config[:repo]) && (params[:ref] == "refs/heads/#{branch}")
-            logger.info "Processing overlay for repo: #{repo_config[:repo]} and branch: #{repo_config[:branch] || 'master'}"
+            logger.info "Enqueueing GithubJob for repo: #{repo_config[:repo]} and branch: #{repo_config[:branch] || 'master'}"
             SuckerPunch::Queue[:github_queue].async.perform repo_config
           end
         end
